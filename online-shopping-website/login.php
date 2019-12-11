824d6289b34cc806d2960e23ecbf2621a37cee20
Jayshree Dey <54076196+jayshreedey@users.noreply.github.com>
2019-12-11 13:08:01 +0530

    Add files via upload

---
+++ online-shopping-website/login.php
@@ -0,0 +1,65 @@
+<?php
+    require 'connection.php';
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Product Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <br><br><br>
+           <div class="container">
+                <div class="row">
+                    <div class="col-xs-6 col-xs-offset-3">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3>LOGIN</h3>
+                            </div>
+                            <div class="panel-body">
+                                <p>Login to make a purchase.</p>
+                                <form method="post" action="login_submit.php">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" name="password" placeholder="Password(minimum 6 characters)" pattern=".{6,}">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" value="Login" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="panel-footer">Don't have an account yet? <a href="signup.php">Register!</a></div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+           <br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center>
+                  
+                   <p>Copyright &copy <a href="https://productstores.in">Productstore | </a>All Rights Reserved.</p>
+                   <p>Jayshree Dey | 2019 </p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
---
+++ online-shopping-website/settings.php
@@ -0,0 +1,63 @@
+<?php
+    session_start();
+    require 'connection.php';
+    if(!isset($_SESSION['email'])){
+        header('location:index.php');
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Product Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-4">
+                        <h1>Change Password</h1>
+                        <form method="post" action="setting_script.php">
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="oldPassword" placeholder="Old Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="newPassword" placeholder="New Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="retype" placeholder="Re-type new password">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Change">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+               <center>
+                   
+                   <p>Copyright &copy <a href="https://productstores.in">Productstore | </a>All Rights Reserved.</p>
+                   <p> Jayshree Dey | 2019</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
